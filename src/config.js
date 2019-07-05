c7b6fe6c3135f3de5344ac0d20a0d4c4a3e2fb32
vishwanath <vishwanath@bigappcomany.in>
2019-07-05 18:21:11 +0530

    React chat app

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export default {
+  appId: "50379574d4f6f5", //Enter your App ID
+  apiKey: "20f24fdc671c44fa7b02cda191107df8b917f59a", //Enter your API KEY
+  GUID: "vishwagroup", // Enter your group UID
+  name: "Family Point"
+};
+
+
+
